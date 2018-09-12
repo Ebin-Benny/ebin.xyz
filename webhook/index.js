160d54692dc137469c68f3a517832343ae5e3521
Ebin Benny <ebin@ebin.xys-server>
2018-09-12 21:36:17 +0000

    Added node webhook listener

---
+++ webhook/index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var childProcess = require('child_process');
+var username = "Ebin-Benny"
+
+app.post("/webhook/github", function(req,res) {
+	var sender = req.body.sender;
+	
+	if(sender.login === username) {
+		deploy(res);
+	}
+});
+
+function deploy(res) {
+	childProcess.exec('cd .. && ./deploy.sh', function(err, stdout, stderr) {
+		if(err) {
+			console.error(err);
+			return res.send(500);
+		}
+		res.send(200);
+	});
+}
