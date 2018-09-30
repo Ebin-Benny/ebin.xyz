fe9c8dead9a8bab7fb2c023d76600170b942e7a5
Ebin Benny <ebin@ebin.xys-server>
2018-09-30 14:24:18 +0000

    Updated webhook server

--- webhook/index.js
+++ webhook/index.js
@@ -1,24 +1,26 @@
+var bodyParser = require('body-parser')
 var express = require("express");
 var app = express();
 var childProcess = require('child_process');
 var username = "Ebin-Benny"
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.listen(8080, () => console.log('Listening on Port 8080'));
 
 app.post("/webhook/github", function(req,res) {
 	var sender = req.body.sender;
-	
-	if(sender.login === username) {
-		deploy(res);
-	}
+        if(sender.login === username) {
+                deploy(res);
+        }
 });
 
 function deploy(res) {
-	childProcess.exec('cd .. && ./deploy.sh', function(err, stdout, stderr) {
+	childProcess.exec('cd .. && sh deploy.sh', function(err, stdout, stderr) {
 		if(err) {
 			console.error(err);
-			return res.send(500);
+			return res.sendStatus(500);
 		}
-		res.send(200);
+		res.sendStatus(200);
 	});
 }
